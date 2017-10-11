cbf0442fdd64960871b6d8ff7aa5065d9470876a
Joseph Dalmasso <me@jodalmasso.com>
2017-10-11 13:18:26 -0400

    changed slider

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,62 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-$(document).ready(function () {
-
-  var width = $(window).width();
-
-  if (width < 600) {
-    $('.slider').bxSlider({
-      maxSlides: 1,
-      minSlides: 1,
-      slideWidth: 450
-    });
-  } else if (width < 1000) {
-    $('.slider').bxSlider({
-      maxSlides: 2,
-      minSlides: 2,
-      slideWidth: 450
-    });
-  } else {
-    $('.slider').bxSlider({
-      maxSlides: 4,
-      minSlides: 4,
-      slideWidth: 450
-    });
-  };
-
-  $(window).on('resize', function () {
-    if (width > 0) {
-      location.reload();
-    }
-  });
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
 });
-
-//sticky menu
-
-// Create a clone of the menu, right next to original.
-$('.menu').addClass('original').clone().insertAfter('.menu').addClass('cloned').css('position', 'fixed').css('top', '0').css('margin-top', '0').css('z-index', '500').removeClass('original').hide();
-
-scrollIntervalID = setInterval(stickIt, 10);
-
-function stickIt() {
-
-  var orgElementPos = $('.original').offset();
-  orgElementTop = orgElementPos.top;
-
-  if ($(window).scrollTop() >= orgElementTop) {
-    // scrolled past the original position; now only show the cloned, sticky element.
-
-    // Cloned element should always have same left position and width as original element.     
-    orgElement = $('.original');
-    coordsOrgElement = orgElement.offset();
-    leftOrgElement = coordsOrgElement.left;
-    widthOrgElement = orgElement.css('width');
-    $('.cloned').css('left', leftOrgElement + 'px').css('top', 0).css('width', widthOrgElement).show();
-    $('.original').css('visibility', 'hidden');
-  } else {
-    // not scrolled past the menu; only show the original menu.
-    $('.cloned').hide();
-    $('.original').css('visibility', 'visible');
-  }
-}
 
 },{}]},{},[1]);
